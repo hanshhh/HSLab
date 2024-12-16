d7bf6cddb56849684156c8f653f062ddf71164e4
hanshhh <wenqi.zhan@mail.utoronto.ca>
2024-12-16 05:00:01 -0500

    initial DB schema

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://deep-polliwog-15.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
---
+++ src/app/about/page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function page() {
+  console.log("hello from about");
+  return <div>page</div>;
+}
