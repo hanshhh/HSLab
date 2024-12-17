4c807f5a3f3afc8cb9766ea8443971a283febee1
hanshhh <wenqi.zhan@mail.utoronto.ca>
2024-12-17 02:16:47 -0500

    minor

--- src/app/(root)/_components/OutputPanel.tsx
+++ src/app/(root)/_components/OutputPanel.tsx
@@ -1,3 +1,16 @@
+"use client";
+import { useCodeEditorStore } from "@/store/useCodeEditorStore";
+import { useState, useEffect } from "react";
 export default function OutputPanel() {
+  const { output, error, isRunning } = useCodeEditorStore();
+  const [isCopied, setIsCopied] = useState(false);
+  const hasContent = error || output;
+
+  const handleCopy = async () => {
+    if (!hasContent) return;
+    await navigator.clipboard.writeText(error || output);
+    setIsCopied(true);
+    setTimeout(() => setIsCopied(false), 2000);
+  };
   return <div>OutputPanel</div>;
 }
