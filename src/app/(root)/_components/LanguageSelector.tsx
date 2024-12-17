fb66efaf769ec344a8b64827e226fbd905925b5f
hanshhh <wenqi.zhan@mail.utoronto.ca>
2024-12-17 01:07:30 -0500

    Setup home layout, global state store

---
+++ src/app/(root)/_components/LanguageSelector.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+interface LanguageSelectorProps {
+  hasAccess: boolean;
+}
+export default function LanguageSelector({ hasAccess }: LanguageSelectorProps) {
+  return <div>LanguageSelector</div>;
+}
